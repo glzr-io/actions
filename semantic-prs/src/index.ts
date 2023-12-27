314b68bb6b8d5db7581263c7b9f6bc8b8d9f2eae
Lars Berger <lars.berger@yahoo.com>
2023-12-27 12:36:54 +0000

    feat: partially implement semantic prs workflow

--- semantic-prs/src/index.ts
+++ semantic-prs/src/index.ts
@@ -1,23 +1,48 @@
 import * as core from '@actions/core';
+import { context } from '@actions/github';
 
 /**
  * The main function for the action.
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
+    const pullRequest = context.payload.pull_request;
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`);
+    if (!pullRequest) {
+      throw new Error('This action can only be run on pull requests.');
+    }
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString());
-    core.debug(new Date().toTimeString());
+    const title: string = pullRequest.title;
+    const types = [
+      'build',
+      'chore',
+      'ci',
+      'docs',
+      'feat',
+      'fix',
+      'perf',
+      'refactor',
+      'revert',
+      'style',
+      'test',
+    ];
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString());
+    const startRegex = new RegExp(`^(${types.join('|')}): `);
+
+    if (startRegex.test(title)) {
+      throw new Error(
+        'Title needs to be of format:\n' +
+          "<type>: <description> (eg. 'feat: added emojis')\n\n" +
+          'Title needs to start with a valid type. These are:\n' +
+          types.join(', '),
+      );
+    }
   } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message);
+    // Fail the workflow run if an error occurs.
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
   }
 }
+
+run();
