3240fbfc84ed8de32f3b30dee98e5f6fff017b70
Lars Berger <lars.berger@yahoo.com>
2023-12-27 12:36:54 +0000

    feat: add github comment on invalid PR title

--- semantic-prs/src/index.ts
+++ semantic-prs/src/index.ts
@@ -1,5 +1,20 @@
 import * as core from '@actions/core';
-import { context } from '@actions/github';
+import { context, getOctokit } from '@actions/github';
+import { WebhookPayload } from '@actions/github/lib/interfaces';
+
+const VALID_CHANGE_TYPES = [
+  'build',
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'fix',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test',
+];
 
 /**
  * The main function for the action.
@@ -13,29 +28,10 @@ export async function run(): Promise<void> {
     }
 
     const title: string = pullRequest.title;
-    const types = [
-      'build',
-      'chore',
-      'ci',
-      'docs',
-      'feat',
-      'fix',
-      'perf',
-      'refactor',
-      'revert',
-      'style',
-      'test',
-    ];
+    const startRegex = new RegExp(`^(${VALID_CHANGE_TYPES.join('|')}): `);
 
-    const startRegex = new RegExp(`^(${types.join('|')}): `);
-
-    if (startRegex.test(title)) {
-      throw new Error(
-        'Title needs to be of format:\n' +
-          "<type>: <description> (eg. 'feat: added emojis')\n\n" +
-          'Title needs to start with a valid type. These are:\n' +
-          types.join(', '),
-      );
+    if (!startRegex.test(title)) {
+      exitWithPrComment(pullRequest);
     }
   } catch (error) {
     // Fail the workflow run if an error occurs.
@@ -43,6 +39,45 @@ export async function run(): Promise<void> {
       core.setFailed(error.message);
     }
   }
+}
+
+async function exitWithPrComment(
+  pullRequest: WebhookPayload['pull_request'],
+): Promise<void> {
+  const message = `\
+    **Title needs to be of format:**
+    \`\`\`
+    <change type>: <short summary>
+      │                   │
+      │                   └─⫸ Summary in present tense. Not capitalized. No period at the end.
+      │
+      └─⫸ Valid change types: ${VALID_CHANGE_TYPES.join('|')}
+    \`\`\`
+
+    eg. 'feat: add option to disable window transition animations'
+
+    **Which change type to choose?**
+    * chore: Changes that affect the build system or external dependencies
+    * ci: Changes to CI configuration files and scripts
+    * docs: Documentation only changes
+    * feat: A new feature
+    * fix: A bug fix
+    * perf: A code change that improves performance
+    * refactor: A code change that neither fixes a bug nor adds a feature
+    * test: Adding missing tests or correcting existing tests
+  `.replace(/(\n)\s+/g, '$1');
+
+  const token = core.getInput('gh-token');
+  const octokit = getOctokit(token);
+
+  await octokit.rest.issues.createComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: pullRequest!.number,
+    body: message,
+  });
+
+  throw new Error(message);
 }
 
 run();
