9da430ebbaf7bde53e521a2ddf81d9033578c82b
Lars Berger <lars.berger@yahoo.com>
2023-12-27 12:36:54 +0000

    feat: allow release branch and gh/npm token to be passed via inputs

--- semantic-release/src/index.ts
+++ semantic-release/src/index.ts
@@ -10,7 +10,7 @@ import { promisify } from 'node:util';
 export async function run(): Promise<void> {
   try {
     if (process.env.CI) {
-      // semantic-release refuses to work when bundled into a single file, so
+      // Semantic Release refuses to work when bundled into a single file, so
       // instead we install it at runtime.
       const { stdout, stderr } = await promisify(exec)(
         'npm --loglevel error i semantic-release@22.0.12',
@@ -27,8 +27,11 @@ export async function run(): Promise<void> {
     const isPrelease = core.getBooleanInput('is_prerelease');
     const prereleaseTag = core.getInput('prerelease_tag');
     const repositoryUrl = core.getInput('repository_url');
-    const npmPublish = core.getBooleanInput('npm_publish');
+    const releaseBranch = core.getInput('release_branch');
     const ghPublish = core.getBooleanInput('gh_publish');
+    const ghToken = core.getInput('gh_token');
+    const npmPublish = core.getBooleanInput('npm_publish');
+    const npmToken = core.getInput('npm_token');
 
     const plugins: PluginSpec[] = [
       '@semantic-release/commit-analyzer',
@@ -52,22 +55,29 @@ export async function run(): Promise<void> {
       ]);
     }
 
-    // Since semantic-release is installed at runtime, it needs to be imported
+    // Since Semantic Release is installed at runtime, it needs to be imported
     // dynamically.
     const semanticRelease = (await import('semantic-release')).default;
 
-    const result = await semanticRelease({
-      branches: [
-        {
-          name: 'main',
-          ...(isPrelease ? { channel: prereleaseTag } : {}),
-          prerelease: isPrelease ? prereleaseTag : false,
-        },
-        { name: '*' },
-      ],
-      repositoryUrl,
-      plugins,
-    });
+    const result = await semanticRelease(
+      {
+        branches: [
+          {
+            name: releaseBranch,
+            ...(isPrelease ? { channel: prereleaseTag } : {}),
+            prerelease: isPrelease ? prereleaseTag : false,
+          },
+          // If there is only a prerelease branch, Semantic Release will error
+          // and require a release branch to be added. We can bypass this by
+          // adding a wildcard release branch, but it means the repository has
+          // to have at least one branch other than `main`.
+          { name: '*' },
+        ],
+        repositoryUrl,
+        plugins,
+      },
+      { env: { GITHUB_TOKEN: ghToken, NPM_TOKEN: npmToken } },
+    );
 
     if (!result) {
       throw new Error('Failed to publish release.');
